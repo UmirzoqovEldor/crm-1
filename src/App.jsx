cbdbdf1fd6941d8e882f61a932b17b486f7365e1
UmirzoqovEldor <umirzoqoveldor46@gmail.com>
2024-02-28 20:39:50 +0500

    use-reduer

--- src/App.jsx
+++ src/App.jsx
@@ -1,157 +1,42 @@
-import { Component } from "react";
+import React from "react";
+import { useReducer,useState } from "react";
 
-import {
-  HeaderDate,
-  Contanier,
-  Div,
-  Emil,
-  Hr,
-  Text,
-  Img,
-  Title,
-  IconName,
-  Analetk,
-  TextName,
-  Name,
-  Header,
-  Navbar,
-  Input,
-  HeaderTime,
-  Am,
-  HeadeSelect,
-} from "./style";
 
-import { GoChevronRight } from "react-icons/go";
-import { CiClock2 } from "react-icons/ci";
-import { IoPerson } from "react-icons/io5";
-import { HiOutlineBriefcase } from "react-icons/hi";
-import { GoChecklist } from "react-icons/go";
-import { FiSettings } from "react-icons/fi";
-import { FiUsers } from "react-icons/fi";
-import { BsBook } from "react-icons/bs";
-import { IoIosSchool } from "react-icons/io";
-import { BsBackpack4Fill } from "react-icons/bs";
+export default function () {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [counter,dispetch] = useReducer((state,action)=>{
+   switch (action.type) {
+    case 'plus':return state+1;
+      
+    case 'minus':return state-1;
+    case 'besh':return state+5;
 
-class Root extends Component {
-  render() {
-    return (
-      <>
-        <Contanier>
-          <Navbar>
-            <Div>
-              {/* <Img src={img}></Img> */}
-              <Title>Webbrain.com</Title>
-            </Div>
-            <Hr></Hr>
-
-            <Div>
-              <Img>
-                <IoPerson />
-              </Img>
-
-              <div>
-                <Text>Webbrain Academiyasi</Text>
-                <Emil>webbrain@gamil.com..</Emil>
-              </div>
-            </Div>
-            <Analetk>
-              <Name>
-                <CiClock2 />
-              </Name>
-              <TextName>Analitik</TextName>
-
-              <IconName>
-                <GoChevronRight />
-              </IconName>
-            </Analetk>
-            <Analetk>
-              <Name>
-                <GoChecklist />
-              </Name>
-              <TextName>Buyurtma</TextName>
-
-              <IconName>
-                <GoChevronRight />
-              </IconName>
-            </Analetk>
-            <Analetk>
-              <Name>
-                <HiOutlineBriefcase />
-              </Name>
-              <TextName>Moliya</TextName>
-
-              <IconName>
-                <GoChevronRight />
-              </IconName>
-            </Analetk>
-            <Analetk>
-              <Name>
-                <IoIosSchool />
-              </Name>
-              <TextName>Talaba</TextName>
-
-              <IconName>
-                <GoChevronRight />
-              </IconName>
-            </Analetk>
-            <Analetk>
-              <Name>
-                <FiUsers />
-              </Name>
-              <TextName>Gurhlar</TextName>
-
-              <IconName>
-                <GoChevronRight />
-              </IconName>
-            </Analetk>
-            <Analetk>
-              <Name>
-                <BsBook />
-              </Name>
-              <TextName>Kurslar</TextName>
-
-              <IconName>
-                <GoChevronRight />
-              </IconName>
-            </Analetk>
-            <Analetk>
-              <Name>
-                <BsBackpack4Fill />
-              </Name>
-              <TextName>Hr</TextName>
-
-              <IconName>
-                <GoChevronRight />
-              </IconName>
-            </Analetk>
-
-            <Analetk>
-              <Name>
-                <FiSettings />
-              </Name>
-              <TextName>Sozlamalar</TextName>
-
-              <IconName>
-                <GoChevronRight />
-              </IconName>
-            </Analetk>
-          </Navbar>
-          <Header>
-            <Input type="text" placeholder="Search..."></Input>
-            <HeaderTime>09:51</HeaderTime>
-
-            <Am>PM</Am>
-            <HeaderDate type="date" placeholder="03/12/2034">
-             
-            </HeaderDate>
-            <HeadeSelect>
-              <option value="uzb">Uzb</option>
-            </HeadeSelect>
-          </Header>
-        </Contanier>
-      </>
-    );
-  }
+    default:return state
+     
+   }
+  },0)
+  return (
+    <div>
+      <h1 style={{ display: "inline" }}>Count</h1>
+      <span style={{fontSize:"30px"}}>{count}</span>
+      <br />
+      <h2 style={{ display: "inline" }}>Nmae</h2>
+      {name}
+      <br />
+      <h2 style={{ display: "inline" }}>Counter</h2>
+      {counter}
+      <input
+        type="text"
+        onChange={({ target }) => setName(target.value)}
+        placeholder="name..."
+      />
+      <br />
+      <button onClick={()=>setCount(count+1)}>+</button>
+      <button onClick={()=>setCount(count-1)}>-</button>
+      <button onClick={()=>dispetch({type:'plus'})}>+</button>
+      <button onClick={()=>dispetch({type:'minus'})}>-</button>
+      <button onClick={()=>dispetch({type:'besh'})}>5</button>
+    </div>
+  );
 }
-
-export default Root;
